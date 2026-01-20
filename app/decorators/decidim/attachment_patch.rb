e0071d08f2d8bd39d78d983f26f0f29cc015437a
barbara oliveira <barbarafictiovore@gmail.com>
2026-01-20 15:36:59 +0100

    fix CI

--- app/decorators/decidim/attachment_patch.rb
+++ app/decorators/decidim/attachment_patch.rb
@@ -3,6 +3,13 @@
 # Patch pour corriger la méthode file_type dans Decidim::Attachment
 # afin d'afficher le bon type MIME sans les query params AWS S3.
 
+# app/decorators/decidim/attachment_patch.rb
+
+module Decidim
+  module AttachmentPatch
+  end
+end
+
 Rails.logger.info "Patch chargé : Decidim::Attachment#file_type"
 
 Decidim::Attachment.class_eval do
